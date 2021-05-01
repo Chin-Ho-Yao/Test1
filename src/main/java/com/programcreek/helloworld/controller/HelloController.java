f64d45f27acb3ed5e4f9b780a92be7d6ffd5d2a7
Chin-Ho-Yao <q82717182q@gmail.com>
2021-05-01 23:51:56 +0800

    加註解

--- src/main/java/com/programcreek/helloworld/controller/HelloController.java
+++ src/main/java/com/programcreek/helloworld/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.programcreek.helloworld.service.MemberService;
 import com.programcreek.helloworld.model.Member;
-
+//https://matthung0807.blogspot.com/2018/05/springmvc-4-hibernate-5-mysql.html
 @Controller
 public class HelloController {
 	@Autowired
